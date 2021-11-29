fc21760ee84dbc5e75c669c4ae2cabd8a0ea7ec7
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2021-11-29 09:31:17 +0700

    fix: rename deploy.js to deployNFT.js

--- ethereum/scripts/deployMyNFT.js
+++ ethereum/scripts/deployMyNFT.js
@@ -3,6 +3,9 @@ async function main() {
 
   // Start deployment, returning a promise that resolves to a contract object
   const myNFT = await MyNFT.deploy()
+
+  await myNFT.deployed()
+
   console.log("Contract deployed to address:", myNFT.address)
 }
 
